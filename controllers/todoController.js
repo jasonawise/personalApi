943305d24af721b6fcd70c2080dc3c331226923f
Jason A. Wise <jasonandrewwise@gmail.com>
2021-11-11 22:19:05 -0500

    setting up some better handling for the req

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose')
 const to_do = mongoose.model('to_do')
 
 exports.addToDo = async (req, res) => {
-  const newTodo = await (new to_do(req.body)).save()
+  console.log(req.body)
 
-  if (newTodo) {
-    res.status(201).send({message: "Successfully added a new todo."})
+  if (Object.keys(req.body).length !== 0) {
+    const newTodo = await (new to_do(req.body)).save()
+      return res.status(201).send({message: "Successfully added a new todo."})
   }
+
+  return res.status(400).send({message: "There is no request body"})
+
 }
